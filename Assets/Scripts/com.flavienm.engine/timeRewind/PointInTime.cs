9bfd1af2ee5274d0250dd8a81450878ae99215f1
Unknown <guillaume.zegoudia@gmail.com>
2017-08-18 17:35:34 +0200

    Rewind Time : ajout stockage velocité des objets (pour éviter qu'il tombe quand on arrete le rewind) (également, début refleixon pour rewind les particles et les instantiates, destroy)

--- Assets/Scripts/com.flavienm.engine/timeRewind/PointInTime.cs
+++ Assets/Scripts/com.flavienm.engine/timeRewind/PointInTime.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
-public class PointInTime {
+public class PointInTime
+{
 
     public Vector3 position;
     public Quaternion rotation;
+    public Vector3 velocity;
 
-    public PointInTime (Vector3 _position, Quaternion _rotation)
+    public PointInTime(Vector3 _position, Quaternion _rotation, Vector3 _velocity)
     {
         position = _position;
         rotation = _rotation;
+        velocity = _velocity;
     }
 
 }
--- Assets/Scripts/com.flavienm.engine/timeRewind/TimeBody.cs
+++ Assets/Scripts/com.flavienm.engine/timeRewind/TimeBody.cs
@@ -4,61 +4,74 @@ using UnityEngine;
 
 public class TimeBody : MonoBehaviour {
 
-	public bool isRewinding;
+    bool isRewinding = false;
 
-	List<PointInTime> pointsInTime;
+    public float recordTime = 5f;
 
-	Rigidbody rb;
-	// Use this for initialization
-	void Start () {
-		pointsInTime = new List<PointInTime>();
-		rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    List<PointInTime> pointsInTime;
 
-		if (Input.GetKeyDown(KeyCode.Return))
-			StartRewind();
-		if (Input.GetKeyUp(KeyCode.Return))
-			StopRewind();
+    Rigidbody rb;
 
+    // Use this for initialization
+    void Start()
+    {
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            StartRewind();
+        if (Input.GetKeyUp(KeyCode.Return))
+            StopRewind();
+    }
 
-	private void FixedUpdate()
-	{
-		if (isRewinding)
-			Rewind();
-		else Record();
-	}
+    void FixedUpdate()
+    {
+        if (isRewinding)
+            Rewind();
+        else
+            Record();
+    }
 
-	void Rewind()
-	{
-		if (pointsInTime.Count > 0)
-		{
-			PointInTime PIT = pointsInTime[0];
-			transform.position = PIT.position;
-			transform.rotation = PIT.rotation;
-			pointsInTime.RemoveAt(0);
-		}
-		else StopRewind();
-	}
+    void Rewind()
+    {
+        if (pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        else
+        {
+            StopRewind();
+        }
 
-	void Record()
-	{
-		pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
-	}
+    }
 
-	void StartRewind()
-	{
-		isRewinding = true;
-		rb.isKinematic = true ;
-	}
+    void Record()
+    {
+        if (pointsInTime.Count > Mathf.Round(recordTime / Time.fixedDeltaTime))
+        {
+            pointsInTime.RemoveAt(pointsInTime.Count - 1);
+        }
 
-	void StopRewind()
-	{
-		isRewinding = false;
-		rb.isKinematic = false;
-	}
+        pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation, rb.velocity));
+    }
+
+    public void StartRewind()
+    {
+        isRewinding = true;
+        rb.isKinematic = true;
+    }
+
+    public void StopRewind()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;
+        rb.velocity = pointsInTime[0].velocity;
+    }
 }
